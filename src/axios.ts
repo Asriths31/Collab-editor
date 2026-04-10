6dc250881f61d7b37db657e89dba5d935d0aaf1b
asriths31 <asrithsai840@gmail.com>
2026-04-10 19:27:09 +0530

    changes base url in axios instance

--- src/axios.ts
+++ src/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 
-// export const baseURL="https://collab-editor-server-fd2i.onrender.com"
-export const baseURL="http://localhost:2000"
+export const baseURL="https://collab-editor-server-fd2i.onrender.com"
+// export const baseURL="http://localhost:2000"
 
 export const axiosInstance = axios.create({
     baseURL:`${baseURL}/backend`,
