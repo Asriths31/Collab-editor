afa58d4033caaa14d0c07bb43b67bd3a6e314cae
asriths31 <asrithsai840@gmail.com>
2026-04-10 14:42:11 +0530

    chaned backend root to deployed service

--- src/axios.ts
+++ src/axios.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 
 
 
+const baseURL="https://collab-editor-server-fd2i.onrender.com"
+// const baseURL="http://localhost:2000/backend"
+
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:2000/backend",
+    baseURL,
     withCredentials:true
 })
